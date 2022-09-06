1b7a5bf1d83f12210b8982b768ffc075b37d48f8
4Furki4 <muhammedcengiz1@gmail.com>
2022-09-06 19:18:27 +0300

    oyunu tekrardan başladan new game butonu new ismi yazacak şekilde eklendi

--- index.js
+++ index.js
@@ -52,13 +52,15 @@ function highlight () {
 
     if (guestScore > homeScore)
     {
+        //underline the guest title and make it red but purge home's title style
         homeTitle.style.color = "#EEEEEE"
-        guestTitle.style.color = "red"
+        guestTitle.style.color = "red" 
         guestTitle.style.textDecoration = "underline"
         homeTitle.style.textDecoration = "none"
     }
     else if(guestScore < homeScore)
     {
+        //the reverse
         homeTitle.style.color = "red"
         guestTitle.style.color = "#EEEEEE"
         guestTitle.style.textDecoration = "none"
@@ -66,10 +68,23 @@ function highlight () {
     }
     else
     {
-        homeTitle.style.color = "#EEEEEE"
-        guestTitle.style.color = "#EEEEEE"
-        guestTitle.style.textDecoration = "none"
-        homeTitle.style.textDecoration = "none"
+        // purge all have been added
+        purgeStyle()
     }
+}
+
+function restart() {
+    guestScore = 0
+    homeScore = 0
+    guestEl.textContent = guestScore;
+    homeEl.textContent = homeScore;
+    purgeStyle()
+}
+
+function purgeStyle(){
+    homeTitle.style.color = "#EEEEEE"
+    guestTitle.style.color = "#EEEEEE"
+    guestTitle.style.textDecoration = "none"
+    homeTitle.style.textDecoration = "none"
 }
 
